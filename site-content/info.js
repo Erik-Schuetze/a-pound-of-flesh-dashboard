15a8bb3904899dcfb42297580760bd746d19e49a
Erik Schuetze <erik.schuetze99@gnail.com>
2024-02-16 23:53:56 +0100

    add help menu for more info

---
+++ site-content/info.js
@@ -0,0 +1,9 @@
+
+document.getElementById('button-info').addEventListener('click', function() {
+    document.getElementById('info-popup').style.display = 'flex';
+});
+
+document.getElementById('button-close-info').addEventListener('click', function() {
+    document.getElementById('info-popup').style.display = 'none';
+});
+
